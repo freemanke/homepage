21dd04f433e8e0c7006ad817d19a8210238813a9
freemanke <freemanke@qq.com>
2022-09-13 17:18:06 +0800

    dev

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 docker build -t hub.infervision.com/kzhiguang/homepage:latest ./
 docker push hub.infervision.com/kzhiguang/homepage:latest
+
+docker tag hub.infervision.com/kzhiguang/homepage:latest home.freemanke.com:60013/freemanke/homepage:latest
+docker push home.freemanke.com:60013/freemanke/homepage:latest
